b55dca792a801f6f4aedd2dbdafcbf358f0d3d1a
zamman <zamman365@gmail.com>
2026-06-06 19:15:54 +0530

    Commit1

--- Main.py
+++ Main.py
@@ -3,3 +3,15 @@ import pathlib as path
 class MovieBooking:
     database="Movies.json"
     data=[]
+    if path(database).exists():
+        with open(database) as Myfile:
+            data=json.load(Myfile)
+    @classmethod
+    def __update(cls):
+        with open(cls.database,"w") as Myfile:
+            Myfile.write(json.dumps(cls.data))
+    def addmovie(self):
+        movie={"movie_name" : input("enter the movie name"),
+               "available_seats" :int(input("seats :"))}
+
+           
